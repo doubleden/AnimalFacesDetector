dee37dc94c6eb0f0668979bbfffa56ae77636ad6
Denis Denisov <denis.denisov8701@gmail.com>
2025-07-18 14:15:28 +0200

    Initial Commit

---
+++ AnimalFacesDetector/AnimalFacesDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFacesDetectorApp.swift
+//  AnimalFacesDetector
+//
+//  Created by Denis Denisov on 18/7/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFacesDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
